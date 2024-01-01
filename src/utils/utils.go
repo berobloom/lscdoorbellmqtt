65e2622c0b2d4ab2eb811c3bf0b316db4dacbab9
Berobloom <berobloom@gmail.com>
2024-01-01 21:53:06 +0100

    Tidy up

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -7,7 +7,6 @@ import (
 )
 
 func GetExecutableSourceDir() string {
-	// get the directory where the executable is located /media/lscdoorbellmqtt
 	dirPath, err := os.Executable()
 	if err != nil {
 		log.Fatal(err)
